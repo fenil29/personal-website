35e3e6d710ac1c66c9c735bf5ef38e043e1a3742
Fenil Kaneria <fenilkaneria99@gmail.com>
2020-01-25 13:23:55 +0530

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Particles from "react-particles-js";
+
+import { Icon, Progress, Spin } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Particles
+          className="particle-background"
+          params={{
+            particles: {
+              number: {
+                value: 50
+              },
+            }
+          }}
+        />
+        <div className="particle-background-content">
+          <div className="home-text-content">
+            <h1>FENIL KANERIA</h1>
+            <h2>UNDER CONSTRUCTION</h2>
+            <h3>WEBSITE IS COMING SOON</h3>
+          </div>
+
+          <div className="home-icons-div">
+            <div className="home-icon">
+              <a href="https://github.com/fenil29">
+                <Icon type="github" className="home-github-icon" />
+              </a>
+            </div>
+            <div className="home-icon">
+              <a href="https://in.linkedin.com/in/fenil-kaneria-2a76bb153">
+                <Icon type="linkedin" className="home-linkedin-icon" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
