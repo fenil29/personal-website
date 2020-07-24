0c3c891b999a93023d7d74af95363bae7f6155bf
fenil29 <fenilkaneria99@gmail.com>
2020-07-24 18:40:29 +0530

    add error and success message on contact form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,58 @@
 import React, { useState } from "react"
 import "./Contact.css"
 import Recaptcha from "react-google-recaptcha"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.min.css"
 
 function Contact() {
   const [captchaVisible, setCaptchaVisible] = useState(false)
   const [formData, setFormData] = useState({})
 
-  const encode = (data) => {
+  const encode = data => {
     return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
   }
-
 
   let handleChange = e => {
     setCaptchaVisible(true)
-    setFormData({...formData,[e.target.name]: e.target.value})
+    setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
   let handleRecaptcha = value => {
-    setFormData({...formData,"g-recaptcha-response": value})
-
+    setFormData({ ...formData, "g-recaptcha-response": value })
   }
   let handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": "contact",
-        ...formData
-      })
+        ...formData,
+      }),
     })
-      .then(() => alert("success"))
-      .catch(error => alert(error));
-  };
+      .then(() => {
+        toast.success("Your submission has been received", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        })
+      })
+      .catch(error =>
+        toast.error("Sorry, something went wrong there. Try again.", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        })
+      )
+  }
   return (
     <div className="contact-container">
       <h1 className="title">Contact me</h1>
@@ -49,30 +67,69 @@ function Contact() {
       >
         <input type="hidden" name="form-name" value="contact" />
         <div className="form-element">
-          <input type="text" name="name" placeholder="Name"  onChange={handleChange} required/>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-element">
-          <input type="email" name="email" placeholder="Email" onChange={handleChange} required/>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-element">
-          <input type="text" name="subject" placeholder="Subject" onChange={handleChange} required/>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-element">
-          <textarea name="message" placeholder="Your Message" onChange={handleChange} required></textarea>
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            onChange={handleChange}
+            required
+          ></textarea>
         </div>
         {/* <div data-netlify-recaptcha="true"></div> */}
-          {captchaVisible && (
+        {captchaVisible && (
           <Recaptcha
-          style={{display:"flex",justifyContent: "center",marginTop:"5px"}}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "5px",
+            }}
             sitekey="6LeFjbUZAAAAAJEgzCJpUJN8_Fpmh3QFlqwCmgI0"
             onChange={handleRecaptcha}
           />
         )}
-   
+
         <div className="form-element button">
           <button type="submit">Send</button>
         </div>
       </form>
+      <ToastContainer
+        className="toast-message"
+        // position="bottom-right"
+        // autoClose={50000}
+        // hideProgressBar={false}
+        // newestOnTop={false}
+        // closeOnClick
+        // rtl={false}
+        // pauseOnFocusLoss
+        // draggable
+        // pauseOnHover
+      />
     </div>
   )
 }
