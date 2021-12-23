5cf42955703a242bc754f377200114d1fa63e9ce
fenil29 <fenilkaneria@gmail.com>
2021-12-23 14:41:17 -0500

    upgrade  deprecated package react-particles-js with react-particles-js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,7 @@ import "./Home.css"
 
 import { Button } from "antd"
 
-import Particles from "react-particles-js"
-import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa"
-import { MdEmail } from "react-icons/md"
+import Particles from "react-tsparticles"
 let colorList = [
   "--primary-color",
   "--primary-background-color",
@@ -13,7 +11,6 @@ let colorList = [
   "--primary-font-color",
   "--secondary-font-color",
 ]
-
 function Home() {
   const [darkMode, setDarkMode] = useState(false)
   let handleDarkModeClick = () => {
@@ -29,26 +26,398 @@ function Home() {
   }
   return (
     <div className="App-header">
+
       <Particles
-        style={{ color: "black" }}
+        id="tsparticles"
         className="particle-background"
-        params={{
-          particles: {
-            number: {
-              value: 20,
-              density: {
-                enable: true,
-                value_area: 800,
+        // init={particlesInit}
+        // loaded={particlesLoaded}
+        options={
+          //   {
+          //   background: {
+          //     color: {
+          //       // value: !darkMode ? "#15202b" : "#ffffff",
+          //     },
+          //   },
+          //   fpsLimit: 60,
+          //   particles: {
+          //     color: darkMode ? "#15202b" : "#ffffff",
+          //   },
+          //   fullScreen:false
+          // }
+          {
+            autoPlay: true,
+            background: {
+              color: {
+                // value: !darkMode ? "#15202b" : "#ffffff",
               },
             },
-            color: {
-              value: darkMode ? "#15202b" : "#ffffff",
+            fpsLimit: 60,
+
+            fullScreen: {
+              enable: true,
+              zIndex: -10,
             },
-            line_linked: {
+            detectRetina: true,
+            duration: 0,
+            fpsLimit: 60,
+
+            particles: {
               color: darkMode ? "#15202b" : "#ffffff",
+              bounce: {
+                horizontal: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0.1,
+                  },
+                  value: 1,
+                },
+                vertical: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0.1,
+                  },
+                  value: 1,
+                },
+              },
+              collisions: {
+                bounce: {
+                  horizontal: {
+                    random: {
+                      enable: false,
+                      minimumValue: 0.1,
+                    },
+                    value: 1,
+                  },
+                  vertical: {
+                    random: {
+                      enable: false,
+                      minimumValue: 0.1,
+                    },
+                    value: 1,
+                  },
+                },
+                enable: false,
+                mode: "bounce",
+                overlap: {
+                  enable: true,
+                  retries: 0,
+                },
+              },
+
+              destroy: {
+                mode: "none",
+                split: {
+                  count: 1,
+                  factor: {
+                    random: {
+                      enable: false,
+                      minimumValue: 0,
+                    },
+                    value: 3,
+                  },
+                  rate: {
+                    random: {
+                      enable: false,
+                      minimumValue: 0,
+                    },
+                    value: {
+                      min: 4,
+                      max: 9,
+                    },
+                  },
+                  sizeOffset: true,
+                },
+              },
+              gradient: [],
+              groups: {
+                z5000: {
+                  number: {
+                    value: 70,
+                  },
+                  zIndex: {
+                    value: 50,
+                  },
+                },
+                z7500: {
+                  number: {
+                    value: 30,
+                  },
+                  zIndex: {
+                    value: 75,
+                  },
+                },
+                z2500: {
+                  number: {
+                    value: 50,
+                  },
+                  zIndex: {
+                    value: 25,
+                  },
+                },
+                z1000: {
+                  number: {
+                    value: 40,
+                  },
+                  zIndex: {
+                    value: 10,
+                  },
+                },
+              },
+              life: {
+                count: 0,
+                delay: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0,
+                  },
+                  value: 0,
+                  sync: false,
+                },
+                duration: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0.0001,
+                  },
+                  value: 0,
+                  sync: false,
+                },
+              },
+              links: {
+                blink: false,
+                color: {
+                  value: "#ffffff",
+                },
+                consent: false,
+                distance: 100,
+                enable: false,
+                frequency: 1,
+                opacity: 0.4,
+                shadow: {
+                  blur: 5,
+                  color: {
+                    value: "#00ff00",
+                  },
+                  enable: false,
+                },
+                triangles: {
+                  enable: false,
+                  frequency: 1,
+                },
+                width: 1,
+                warp: false,
+              },
+              move: {
+                angle: {
+                  offset: 0,
+                  value: 10,
+                },
+                attract: {
+                  distance: 200,
+                  enable: false,
+                  rotate: {
+                    x: 600,
+                    y: 1200,
+                  },
+                },
+                decay: 0,
+                distance: {},
+                direction: "right",
+                drift: 0,
+                enable: true,
+                gravity: {
+                  acceleration: 9.81,
+                  enable: false,
+                  inverse: false,
+                  maxSpeed: 50,
+                },
+                path: {
+                  clamp: true,
+                  delay: {
+                    random: {
+                      enable: false,
+                      minimumValue: 0,
+                    },
+                    value: 0,
+                  },
+                  enable: false,
+                  options: {},
+                },
+                outModes: {
+                  default: "out",
+                  bottom: "out",
+                  left: "out",
+                  right: "out",
+                  top: "out",
+                },
+                random: false,
+                size: false,
+                speed: 1,
+                spin: {
+                  acceleration: 0,
+                  enable: false,
+                },
+                straight: false,
+                trail: {
+                  enable: false,
+                  length: 10,
+                  fillColor: {
+                    value: "#000000",
+                  },
+                },
+                vibrate: false,
+                warp: false,
+              },
+              number: {
+                density: {
+                  enable: false,
+                  area: 800,
+                  factor: 1000,
+                },
+                limit: 0,
+                value: 200,
+              },
+              opacity: {
+                random: {
+                  enable: false,
+                  minimumValue: 0.1,
+                },
+                value: 1,
+                animation: {
+                  count: 0,
+                  enable: false,
+                  speed: 3,
+                  sync: false,
+                  destroy: "none",
+                  startValue: "random",
+                  minimumValue: 0.1,
+                },
+              },
+              orbit: {
+                animation: {
+                  count: 0,
+                  enable: false,
+                  speed: 1,
+                  sync: false,
+                },
+                enable: false,
+                opacity: 1,
+                rotation: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0,
+                  },
+                  value: 45,
+                },
+                width: 1,
+              },
+              reduceDuplicates: false,
+              repulse: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: 0,
+                enabled: false,
+                distance: 1,
+                duration: 1,
+                factor: 1,
+                speed: 1,
+              },
+              roll: {
+                darken: {
+                  enable: false,
+                  value: 0,
+                },
+                enable: false,
+                enlighten: {
+                  enable: false,
+                  value: 0,
+                },
+                mode: "vertical",
+                speed: 25,
+              },
+              rotate: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: 0,
+                animation: {
+                  enable: false,
+                  speed: 0,
+                  sync: false,
+                },
+                direction: "clockwise",
+                path: false,
+              },
+              shadow: {
+                blur: 0,
+                color: {
+                  value: "#000000",
+                },
+                enable: false,
+                offset: {
+                  x: 0,
+                  y: 0,
+                },
+              },
+              shape: {
+                options: {},
+                type: "circle",
+              },
+              size: {
+                random: {
+                  enable: false,
+                  minimumValue: 1,
+                },
+                value: 3,
+                animation: {
+                  count: 0,
+                  enable: false,
+                  speed: 5,
+                  sync: false,
+                  destroy: "none",
+                  startValue: "random",
+                },
+              },
+              stroke: {
+                width: 0,
+              },
+              tilt: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: 0,
+                animation: {
+                  enable: false,
+                  speed: 0,
+                  sync: false,
+                },
+                direction: "clockwise",
+                enable: false,
+              },
+              twinkle: {
+                lines: {
+                  enable: false,
+                  frequency: 0.05,
+                  opacity: 1,
+                },
+                particles: {
+                  enable: false,
+                  frequency: 0.05,
+                  opacity: 1,
+                },
+              },
+              wobble: {
+                distance: 5,
+                enable: false,
+                speed: 50,
+              },
             },
-          },
-        }}
+          }
+        }
       />
       <div className="dark-mode-icon" onClick={handleDarkModeClick}>
         {!darkMode ? (
@@ -80,40 +449,8 @@ function Home() {
           <h2>Software Engineer</h2>
           {/* <h2>UNDER CONSTRUCTION</h2>
           <h3>WEBSITE IS COMING SOON</h3> */}
-       
-            <div className="icon-container">
-              <a
-                href="https://github.com/fenil29"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaGithub className="icon" />
-              </a>
-              <a
-                href="https://in.linkedin.com/in/fenil-kaneria-2a76bb153"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedin className="icon" />
-              </a>
-              <a
-                href="https://twitter.com/FenilKaneria"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaTwitter className="icon" />
-              </a>
-              <a
-                href="mailto:fenilkaneria@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <MdEmail className="icon" style={{ fontSize: "38px" }} />
-              </a>
-            </div>
-            <a href="/Fenil_Kaneria_Resume.pdf" target="_blank" rel="noopener noreferrer" className="resume-button">
-            {/* <Button>My Resume</Button> */}
-            Resume
+          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            <Button>My Resume</Button>
           </a>
         </div>
       </div>
