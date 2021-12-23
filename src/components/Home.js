139d7aac483692d29370a48278a4489b91193c11
fenil29 <fenilkaneria@gmail.com>
2021-12-23 14:51:43 -0500

    revert back to hint: as appropriate to mark resolution and make a commit.

--- src/components/Home.js
+++ src/components/Home.js
@@ -449,8 +449,45 @@ function Home() {
           <h2>Software Engineer</h2>
           {/* <h2>UNDER CONSTRUCTION</h2>
           <h3>WEBSITE IS COMING SOON</h3> */}
+<<<<<<< HEAD
           <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
             <Button>My Resume</Button>
+=======
+       
+            <div className="icon-container">
+              <a
+                href="https://github.com/fenil29"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub className="icon" />
+              </a>
+              <a
+                href="https://in.linkedin.com/in/fenil-kaneria-2a76bb153"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin className="icon" />
+              </a>
+              <a
+                href="https://twitter.com/FenilKaneria"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter className="icon" />
+              </a>
+              <a
+                href="mailto:fenilkaneria@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdEmail className="icon" style={{ fontSize: "38px" }} />
+              </a>
+            </div>
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="resume-button">
+            {/* <Button>My Resume</Button> */}
+            Resume
+>>>>>>> parent of 2b2cdea (change resume name)
           </a>
         </div>
       </div>
