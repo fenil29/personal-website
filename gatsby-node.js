3bf7926ef42948886001f878516cd781df27cf29
fenil29 <fenilkaneria@gmail.com>
2022-05-05 01:13:07 -0400

    finally fix bug in dynamic route for each project

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,12 +2,13 @@ const path = require("path")
 
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
-  console.log("Page - " + page.page)
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
   if (page.path.match(/^\/project/)) {
-    createPage({
-      path: "/project",
-      matchPath: "/project/:name",
-      component: path.resolve("src/pages/project.js"),
-    })
+    page.matchPath = "/project/*"
+
+    // Update the page.
+    createPage(page)
   }
 }
