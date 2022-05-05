bb4a1662180e9904639face5066d05328bad8c2a
fenil29 <fenilkaneria@gmail.com>
2022-05-05 00:22:46 -0400

    trying to fix bug website not working in production

--- src/pages/project.js
+++ src/pages/project.js
@@ -309,15 +309,7 @@ function Project({ name }) {
          </div>
       ) : (
 
-        <div className="dynamic-project-not-found">
-          <div className="not-found">
-            <h1>404</h1>
-            <h2>NOT FOUND</h2>
-            <div className="text">
-              You just hit a route that doesn&#39;t exist...
-            </div>
-          </div>
-        </div>
+        <h1>404 Not Found</h1>
       )}
       <Footer />
     </>
