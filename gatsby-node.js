11f9540fc8d06e93bba224b264c06fa6e33c4023
fenil29 <fenilkaneria@gmail.com>
2022-05-06 01:05:21 -0400

    bug fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,13 +2,12 @@ const path = require("path")
 
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
-
-  // page.matchPath is a special key that's used for matching pages
-  // only on the client.
+  console.log("Page - " + page.page)
   if (page.path.match(/^\/project/)) {
-    page.matchPath = "/project/*"
-
-    // Update the page.
-    createPage(page)
+    createPage({
+      path: "/project",
+      matchPath: "/project/:name",
+      component: path.resolve("src/pages/project.js"),
+    })
   }
 }
