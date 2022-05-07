fc6c76d0d938f15c32c36307638409b9549b1cf7
fenil29 <fenilkaneria@gmail.com>
2022-05-07 16:47:56 -0400

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,37 @@
-import { createGetInitialProps } from "@mantine/next";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { createGetInitialProps } from '@mantine/next'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-const getInitialProps = createGetInitialProps();
+const getInitialProps = createGetInitialProps()
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+  static getInitialProps = getInitialProps
 
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
