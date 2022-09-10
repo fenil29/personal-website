208a430d56d38f8cc72d531e88d591313ed96d6f
fenil29 <fenilkaneria@gmail.com>
2022-09-10 16:30:04 -0400

    add Nuance work experience

--- components/EandE.js
+++ components/EandE.js
@@ -8,71 +8,84 @@ function EandE() {
       <div className={styles.timelineContainer}>
         <Timeline lineWidth={3} radius="md">
           <Timeline.Item
-            title="Master of Applied Computing"
-            bulletSize={40}
-            bullet={<Book2 size={20} />}
+            title="Front-End Development Intern"
+            bulletSize={50}
+            bullet={<Building size={25} />}
             className={styles.timelineItem}
           >
-            <Text color="dimmed" size="sm">
+            <Text color="dimmed" size="md">
+              Nuance Communications
+            </Text>
+            <Text size="md" mt={4}>
+              Sep 2022 - Present
+            </Text>
+          </Timeline.Item>
+          <Timeline.Item
+            title="Master of Applied Computing"
+            bulletSize={50}
+            bullet={<Book2 size={25} />}
+            className={styles.timelineItem}
+          >
+            <Text color="dimmed" size="md">
               University of Windsor
             </Text>
-            <Text size="xs" mt={4}>
+            <Text size="md" mt={4}>
               Sep 2021 - Present
             </Text>
           </Timeline.Item>
 
           <Timeline.Item
             title="Technology Intern"
-            bulletSize={40}
-            bullet={<Building size={20} />}
+            bulletSize={50}
+            bullet={<Building size={25} />}
             className={styles.timelineItem}
           >
-            <Text color="dimmed" size="sm">
+            <Text color="dimmed" size="md">
               Thomson Reuters
             </Text>
-            <Text size="xs" mt={4}>
+            <Text size="md" mt={4}>
               Jan 2021 – June 2021
             </Text>
           </Timeline.Item>
 
           <Timeline.Item
             title="Software Developer Intern"
-            bulletSize={40}
-            bullet={<Building size={20} />}
+            bulletSize={50}
+            bullet={<Building size={25} />}
             className={styles.timelineItem}
           >
-            <Text color="dimmed" size="sm">
+            <Text color="dimmed" size="md">
               Ways Software
             </Text>
-            <Text size="xs" mt={4}>
+            <Text size="md" mt={4}>
               Apr 2020 - Jul 2020
             </Text>
           </Timeline.Item>
 
           <Timeline.Item
             title="Software Developer Intern"
-            bulletSize={40}
-            bullet={<Building size={20} />}
+            bulletSize={50}
+            bullet={<Building size={25} />}
             className={styles.timelineItem}
           >
-            <Text color="dimmed" size="sm">
+            <Text color="dimmed" size="md">
               Bhavi Technologies
             </Text>
-            <Text size="xs" mt={4}>
+            <Text size="md" mt={4}>
               May 2019 - Jun 2019
             </Text>
           </Timeline.Item>
 
           <Timeline.Item
             title="Bachelor of Technology in Computer Science and Engineering"
-            bulletSize={40}
-            bullet={<Book2 size={20} />}
+            bulletSize={50}
+            bullet={<Book2 size={25} />}
             className={styles.timelineItem}
           >
-            <Text color="dimmed" size="sm">
+            <Text color="dimmed" size="md">
               Charusat University
             </Text>
-            <Text size="xs" mt={4}>
+            <Text size="md" mt={4}>
               June 2017 – June 2021
             </Text>
           </Timeline.Item>
