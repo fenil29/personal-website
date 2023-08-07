3e83545c576dd16b777eeb95909e63e04c1dfed3
fenil29 <fenilkaneria@gmail.com>
2023-08-06 22:12:47 -0400

    add new experience

--- components/EandE.js
+++ components/EandE.js
@@ -7,6 +7,20 @@ function EandE() {
       <h1 className="title">Experience and Education</h1>
       <div className={styles.timelineContainer}>
         <Timeline lineWidth={3} radius="md">
+        <Timeline.Item
+            title="Front-End Development Intern"
+            bulletSize={50}
+            bullet={<Building size={25} />}
+            className={styles.timelineItem}
+          >
+            <Text color="dimmed" size="md">
+              BlueCat
+            </Text>
+            <Text size="md" mt={4}>
+              Feb 2023 - Present
+            </Text>
+          </Timeline.Item>
+
           <Timeline.Item
             title="Front-End Development Intern"
             bulletSize={50}
@@ -17,7 +31,7 @@ function EandE() {
               Nuance Communications
             </Text>
             <Text size="md" mt={4}>
-              Sep 2022 - Present
+              Sep 2022 - Dec 2022
             </Text>
           </Timeline.Item>
           <Timeline.Item
@@ -30,7 +44,7 @@ function EandE() {
               University of Windsor
             </Text>
             <Text size="md" mt={4}>
-              Sep 2021 - Present
+              Sep 2021 - Dec 2022
             </Text>
           </Timeline.Item>
 
@@ -45,34 +59,6 @@ function EandE() {
             </Text>
             <Text size="md" mt={4}>
               Jan 2021 – June 2021
-            </Text>
-          </Timeline.Item>
-
-          <Timeline.Item
-            title="Software Developer Intern"
-            bulletSize={50}
-            bullet={<Building size={25} />}
-            className={styles.timelineItem}
-          >
-            <Text color="dimmed" size="md">
-              Ways Software
-            </Text>
-            <Text size="md" mt={4}>
-              Apr 2020 - Jul 2020
-            </Text>
-          </Timeline.Item>
-
-          <Timeline.Item
-            title="Software Developer Intern"
-            bulletSize={50}
-            bullet={<Building size={25} />}
-            className={styles.timelineItem}
-          >
-            <Text color="dimmed" size="md">
-              Bhavi Technologies
-            </Text>
-            <Text size="md" mt={4}>
-              May 2019 - Jun 2019
             </Text>
           </Timeline.Item>
 
